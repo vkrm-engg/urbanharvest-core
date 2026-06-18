01b0ed4c22eb9b3a307431b92984f92cb52d3fdb
vkrm-engg <vikramkannanc10@gmail.com>
2026-06-18 19:14:10 +0530

    Delete frontend directory

--- frontend/node_modules/@cesium/engine/Source/Shaders/Builtin/Constants/passCesium3DTileEdgesDirect.glsl
+++
@@ -1,9 +0,0 @@
-/**
- * The automatic GLSL constant for {@link Pass#CESIUM_3D_TILE_EDGES_DIRECT}
- *
- * @name czm_passCesium3DTileEdgesDirect
- * @glslConstant
- *
- * @see czm_pass
- */
-const float czm_passCesium3DTileEdgesDirect = 12.0;
--- frontend/node_modules/cesium/lint-staged.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  "*.{js,cjs,mjs,ts,tsx,css,html}": [
-    "eslint --cache --quiet",
-    "prettier --write",
-  ],
-  "*.md": ["markdownlint", "prettier --write"],
-};
